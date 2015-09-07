a9cab5cdf5af46f3ebddd3e3fd0efbcc62518bd1
Hani <boustanihani@gmail.com>
2015-09-07 22:04:37 +0000

    xxx

--- autoform-materialize-select.js
+++ autoform-materialize-select.js
@@ -1,23 +1,33 @@
 if (Meteor.isClient) {
 
+    var totalValues = 5;
+
     AutoForm.setDefaultTemplate('materialize');
+
 
     generateValues = function() {
         var allowedValues = [];
-        for (var i = 1; i <= 5; i++) {
+        for (var i = 1; i <= totalValues; i++) {
             allowedValues.push(Random.id([10]).toUpperCase());
         }
         Session.set('allowedValues', allowedValues);
-        Session.set('currentValue', allowedValues[2]);
     };
     generateValues();
 
+    var index = 0;
+
+    randomSelect = function() {
+        index = (index + 1) % totalValues;
+        console.log('index=' + index);
+        Session.set('index', index);
+    };
+
     Template.registerHelper('currentDoc', function() {
         return {
-            mySelect: Session.get('currentValue')
+            mySelect: Session.get('allowedValues')[Session.get('index')]
         };
     });
-    
+
     mySchema = new SimpleSchema({
         mySelect: {
             type: String,
